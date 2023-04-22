da6b6c9ff67d7a111d60ff2d50bfa785fb321865
AlexanderHel <99399435+AlexanderHel@users.noreply.github.com>
2023-04-22 21:34:11 +0700

    Update Xcity to English site

--- scrapinglib/xcity.py
+++ scrapinglib/xcity.py
@@ -17,28 +17,28 @@ class Xcity(Parser):
     expr_actorphoto = '//div[@class="frame"]/div/p/img/@src'
     expr_studio = '//*[@id="avodDetails"]/div/div[3]/div[2]/div/ul[1]/li[4]/a/span/text()'
     expr_studio2 = '//strong[contains(text(),"片商")]/../following-sibling::span/a/text()'
-    expr_runtime = '//span[@class="koumoku" and text()="収録時間"]/../text()'
+    expr_runtime = '//span[@class="koumoku" and text()="Running Time"]/../text()'
     expr_label = '//*[@id="avodDetails"]/div/div[3]/div[2]/div/ul[1]/li[5]/a/span/text()'
     expr_release = '//*[@id="avodDetails"]/div/div[3]/div[2]/div/ul[1]/li[2]/text()'
-    expr_tags = '//span[@class="koumoku" and text()="ジャンル"]/../a[starts-with(@href,"/avod/genre/")]/text()'
+    expr_tags = '//span[@class="koumoku" and text()="Genres"]/../a[starts-with(@href,"/avod/genre/")]/text()'
     expr_cover = '//*[@id="avodDetails"]/div/div[3]/div[1]/p/a/@href'
     expr_director = '//*[@id="program_detail_director"]/text()'
-    expr_series = "//span[contains(text(),'シリーズ')]/../a/span/text()"
-    expr_series2 = "//span[contains(text(),'シリーズ')]/../span/text()"
+    expr_series = "//span[contains(text(),'Series')]/../a/span/text()"
+    expr_series2 = "//span[contains(text(),'Series')]/../span/text()"
     expr_extrafanart = '//div[@id="sample_images"]/div/a/@href'
     expr_outline = '//head/meta[@property="og:description"]/@content'
 
     def queryNumberUrl(self, number):
         xcity_number = number.replace('-','')
         query_result, browser = get_html_by_form(
-            'https://xcity.jp/' + secrets.choice(['sitemap/','policy/','law/','help/','main/']),
+            'https://xxx.xcity.jp/' + secrets.choice(['sitemap/','policy/','law/','help/','main/']),
             fields = {'q' : xcity_number.lower()},
             cookies=self.cookies, proxies=self.proxies, verify=self.verify,
             return_type = 'browser')
         if not query_result or not query_result.ok:
             raise ValueError("xcity.py: page not found")
         prelink = browser.links('avod\/detail')[0]['href']
-        return urljoin('https://xcity.jp', prelink)
+        return urljoin('https://xxx.xcity.jp', prelink)
 
     def getStudio(self, htmltree):
         return super().getStudio(htmltree).strip('+').replace("', '", '').replace('"', '')
@@ -72,13 +72,13 @@ class Xcity(Parser):
         t = {i.text.strip(): i.attrib['href'] for i in treea}
         o = {}
         for k, v in t.items():
-            actorpageUrl = "https://xcity.jp" + v
+            actorpageUrl = "https://xxx.xcity.jp" + v
             try:
                 adtree = self.getHtmlTree(actorpageUrl)
                 picUrl = self.getTreeElement(adtree, self.expr_actorphoto)
                 if 'noimage.gif' in picUrl:
                     continue
-                o[k] = urljoin("https://xcity.jp", picUrl)
+                o[k] = urljoin("https://xxx.xcity.jp", picUrl)
             except:
                 pass
         return o
